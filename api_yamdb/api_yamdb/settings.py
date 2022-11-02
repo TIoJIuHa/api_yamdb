2fefd91b5668fa404c14b2dfe26112a1b37bee60
Полина Елистратова <p.elistratova03@mail.ru>
2022-11-02 11:28:40 +0300

    update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,10 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'api',
-    'reviews',
-    'users',
 ]
 
 MIDDLEWARE = [
