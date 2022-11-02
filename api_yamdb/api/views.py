7c7938b02978a4fbfa41916f1b46f696a4bc9702
Ратмир Саидов <ratmir.saidov@gmail.com>
2022-11-02 17:48:23 +0300

    Добавил модели в reviews, добавил вьюсеты и сериалайзеры

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from reviews.models import Titles, Genre, Category
+from .serializers import TitlesSerializer, GenreSerializer, CategorySerializer
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    '''Вьюсет для модели Категории. Читать может любой пользователь'''
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    '''Вьюсет для модели Жанры. Читать может любой пользователь'''
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitlesViewSet(viewsets.ReadOnlyModelViewSet):
+    '''Вьюсет для Произведений. Читать может любой пользователь'''
+    queryset = Titles.objects.all()
+    serializer_class = TitlesSerializer
