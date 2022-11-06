c0e7d1ff100afe73b7552330b286a1ea68113ad5
Дмитрий Шапченко <dmitryshapchenko@yandex.ru>
2022-11-06 11:32:05 +0300

    refix

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -74,14 +74,12 @@ class ReviewViewSet(viewsets.ModelViewSet):
     pagination_class = LimitOffsetPagination
     
     def perform_create(self, serializer):
-        title_id = self.kwargs.get("title_id")
-        title = get_object_or_404(Title, id=title_id)
+        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
         serializer.save(user=self.request.user, title=title)
 
     def get_queryset(self):
-        title_id = self.kwargs.get("title_id")
-        title = get_object_or_404(Title, id=title_id)
-        return title.reviews
+        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
+        return title.reviews.all()
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -92,12 +90,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
-        review_id = self.kwargs.get("review_id")
-        title_id = self.kwargs.get("title_id")
-        review = get_object_or_404(Review, id=review_id, title=title_id)
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get("review_id"),
+            title=self.kwargs.get("title_id")
+        )
         serializer.save(user=self.request.user, review=review)
 
     def get_queryset(self):
-        review_id = self.kwargs.get("review_id")
-        review = get_object_or_404(Review, id=review_id)
-        return review.comments
+        review = get_object_or_404(Review, id=self.kwargs.get("review_id"))
+        return review.comments.all()
